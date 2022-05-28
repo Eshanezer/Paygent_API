6cec0e076393768e622bf4d56fcaf222ff6d83c4
Sandeepa-Ceylonsoft <sandeepa@ceylonsoft.jp>
2022-05-28 21:50:01 +0530

    oidc

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ResourcesController;
 use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 // Route::prefix('/resources')->controller(ResourcesController::class)->group(function () {
 //     Route::get('/prefectures/{language}/{output}', 'prefectures');
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/auth')->controller(AuthController::class)->group(function () {
     Route::post('/login', 'login');
     Route::post('/register', 'register');
+});
+
+Route::post('/retrieve/access/token', function(Request $request) {
+    // return $request;
+    $res = Http::withHeaders([
+        'Authorization' => 'Bearer '. $request->access_token
+    ])
+    ->get('https://login2.jleague.jp/userinfo');
+    return $res;
 });
 
 Route::get('/check',[TestController::class,'check']);

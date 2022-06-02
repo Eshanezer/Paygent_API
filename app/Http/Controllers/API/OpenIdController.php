ace48d6b3c137c7cc2c8ecc3b09cd1157a7531cb
Pasindu Priyashan <47271150+bataishere@users.noreply.github.com>
2022-06-02 11:35:32 +0530

    check6

--- app/Http/Controllers/API/OpenIdController.php
+++ app/Http/Controllers/API/OpenIdController.php
@@ -33,27 +33,27 @@ class OpenIdController extends Controller
 
         //openid user data 1
         $jLeagueUserCredentials = $this->getJLeagueUserInfo($oauthCredentials);
-        $sfdcUserData = $this->sfdcInterface->getSFDCUserData($jLeagueUserCredentials['mkdb_id'], $this->sfdcInterface->getSFDCOAuthToken()['access_token'])['Return_Data'][0]['Contact'];
-        $this->mkdbInterface->storeUserData([
-            'request_no' => 'login',
-            'entry_type' => 'non',
-            'club_cd' => $jLeagueUserCredentials['club_cd'],
-            'card_type' => 'non',
-            'mkdb_id' => $jLeagueUserCredentials['mkdb_id'],
-            'post_cd' => $sfdcUserData['POST_CD'],
-            'address_state' => $sfdcUserData['ADDRESS_STREET'],
-            'address_city' => $sfdcUserData['ADDRESS_CITY'],
-            'address_street' => $sfdcUserData['ADDRESS_STREET'],
-            'address_building' => $jLeagueUserCredentials['address_building'],
-            'lastname' => $sfdcUserData['FirstName'],
-            'firstname' => $sfdcUserData['LastName'],
-            'member_last_nm_kana' => $sfdcUserData['MEMBER_LAST_NM_KANA'],
-            'member_first_nm_kana' => $sfdcUserData['MEMBER_FIRST_NM_KANA'],
-            'sex' => $sfdcUserData['SEX'],
-            'birthdate' => $sfdcUserData['Birthdate'],
-            'mobilephone' => $sfdcUserData['mkdb_Phoneid'],
-            'email' => $sfdcUserData['Email']
-        ]);
+        // $sfdcUserData = $this->sfdcInterface->getSFDCUserData($jLeagueUserCredentials['mkdb_id'], $this->sfdcInterface->getSFDCOAuthToken()['access_token'])['Return_Data'][0]['Contact'];
+        // $this->mkdbInterface->storeUserData([
+        //     'request_no' => 'login',
+        //     'entry_type' => 'non',
+        //     'club_cd' => $jLeagueUserCredentials['club_cd'],
+        //     'card_type' => 'non',
+        //     'mkdb_id' => $jLeagueUserCredentials['mkdb_id'],
+        //     'post_cd' => $sfdcUserData['POST_CD'],
+        //     'address_state' => $sfdcUserData['ADDRESS_STREET'],
+        //     'address_city' => $sfdcUserData['ADDRESS_CITY'],
+        //     'address_street' => $sfdcUserData['ADDRESS_STREET'],
+        //     'address_building' => $jLeagueUserCredentials['address_building'],
+        //     'lastname' => $sfdcUserData['FirstName'],
+        //     'firstname' => $sfdcUserData['LastName'],
+        //     'member_last_nm_kana' => $sfdcUserData['MEMBER_LAST_NM_KANA'],
+        //     'member_first_nm_kana' => $sfdcUserData['MEMBER_FIRST_NM_KANA'],
+        //     'sex' => $sfdcUserData['SEX'],
+        //     'birthdate' => $sfdcUserData['Birthdate'],
+        //     'mobilephone' => $sfdcUserData['mkdb_Phoneid'],
+        //     'email' => $sfdcUserData['Email']
+        // ]);
         $data = compact('jLeagueUserCredentials', 'oauthCredentials');
         return $this->successResponse(data:$data);
         //end openid user data

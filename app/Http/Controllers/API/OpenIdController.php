260a1434a35ed3f444e2138f48e226d028eead4a
Pasindu Priyashan <47271150+bataishere@users.noreply.github.com>
2022-06-03 14:49:16 +0530

    test15

--- app/Http/Controllers/API/OpenIdController.php
+++ app/Http/Controllers/API/OpenIdController.php
@@ -36,8 +36,7 @@ class OpenIdController extends Controller
         $oauthCredentials =  $this->openIdInterface->getOauthToken($code);
         $jLeagueUserCredentials = $this->getJLeagueUserInfo($oauthCredentials);
         $sfdcDataContact=$this->sfdcInterface->getSFDCUserData($jLeagueUserCredentials['mkdb_id'], $this->sfdcInterface->getSFDCOAuthToken()['access_token'])['Return_Data'][0];
-        return $sfdcDataContact['Contact'];
-        $sfdcUserData = $this->sfdcInterface->getSFDCUserData($jLeagueUserCredentials['mkdb_id'], $this->sfdcInterface->getSFDCOAuthToken()['access_token'])['Return_Data'][0]->Contact;
+        $sfdcUserData = $sfdcDataContact['Contact'];
 
         $user=$this->mkdbInterface->storeUserData([
             'request_no' => 'login',

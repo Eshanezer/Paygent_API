4a40407f234053d9de427140b09185bcd479d4b8
Sandeepa-Ceylonsoft <sandeepa@ceylonsoft.jp>
2022-06-13 14:08:40 +0530

    log

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return config('openIdConfig.endpoints.get_oauth_token_endpoint');
+    Log::info(123);
     return view('welcome');
 });
 
